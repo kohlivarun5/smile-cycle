06a1dc392974ce93211f724e91d21205354cc78b
rgoyal23 <35884752+rgoyal23@users.noreply.github.com>
2018-01-29 00:17:55 +0530

    Update calculate_arb.py

--- calculate_arb.py
+++ calculate_arb.py
@@ -38,5 +38,40 @@ def coinbase_coindelta():
 
     return result
 
+def binance_kucoin():
+    result = []
+    fees = {}
+    fees['binance'] = binance.get_fees()
+    prices = {}
+    prices['binance'] = binance.get_prices()
+    prices['kucoin'] = kucoin.get_prices()
+    buy_amts = {}
+    buy_amts['btc'] = 0.1
+    buy_amts['inr'] = 50000
+    buy_amts['usd'] = 1000
+    for c in prices['binance']['bid'].keys():
+        cur1 = c.split('_')[0]
+        cur2 = c.split('_')[1]
+        if (cur2 == 'btc'):
+            buy_amts[cur1] = 0.1/prices['binance']['bid'][c]
+    for i in prices.keys():
+        for j in prices.keys():
+            if (i == j):
+                continue
+            set1 = set(prices[i]['bid'].keys())
+            set2 = set(prices[j]['bid'].keys())
+            curs = set1.intersection(set2)
+            for cur in curs:
+                cur1 = cur.split('_')[0]
+                cur2 = cur.split('_')[1]
+                buy_amt = buy_amts.get(cur2, 1) # assuming buy amt as 1 in cur2 terms if not found
+                buy = buy_amt/prices[i]['ask'][cur]
+                fee = fees['binance'].get(cur1, 1) # assuming withdrawal fee as 1 if not found
+                sell = (buy - fee) * prices[j]['bid'][cur]
+                if ((sell - buy_amt)*100 > 2*buy_amt): # appending only if 2% profit
+                    result.append({'from' : i, 'to' : j, 'coin' : cur, 'gain_perc' : (sell - buy_amt)*100/buy_amt})
+    return result
+
+
 if __name__ == "__main__":
     print(coinbase_coindelta())
