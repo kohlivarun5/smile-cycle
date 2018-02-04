e88d975faf5bf15b23878df33017abd48d427694
Varun Kohli <kohli.varun5@gmail.com>
2018-02-04 16:39:49 -0500

    .

--- exchanges/forex.py
+++ exchanges/forex.py
@@ -1,16 +1,17 @@
 import json
 import urllib2
 
-def query():
-  url = "https://api.fixer.io/latest"
-  response = urllib2.urlopen(url, timeout = 5)
+def query(base,cross):
+  url = "https://api.fixer.io/latest?symbols=%s&base=%s" % (cross,base)
+  response = urllib2.urlopen(url, timeout = 10)
   return json.load(response)
 
 def get_prices():
-  exch_rates=query()
+  exch_rates=query("USD","INR")
   prices = {}
   prices['bid'] = {}
   prices['ask'] = {}
-  prices['bid']['usd_inr'] = float(exch_rates['rates']['INR'])/float(exch_rates['rates']['USD'])
-  prices['ask']['usd_inr'] = float(exch_rates['rates']['INR'])/float(exch_rates['rates']['USD'])
+  usd_inr = float(exch_rates['rates']['INR'])
+  prices['bid']['usd_inr'] = usd_inr
+  prices['ask']['usd_inr'] = usd_inr
   return prices
