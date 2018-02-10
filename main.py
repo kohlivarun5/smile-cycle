94742c967d189f4d76415a5246a5bf5f2ee08e86
Varun Kohli <kohli.varun5@gmail.com>
2018-02-09 22:05:59 -0500

    .

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def replyToTelegram(msg,chat_id,message_id):
     logging.info('send response:')
     logging.info(resp)
 
-def enqueueTxTask(tx,chat_id,message_id,max_count=30,count=0,countdown=30):
+def enqueueTxTask(tx,chat_id,message_id,max_count=30,count=0,countdown=60):
     if tx.network.status == "confirmed" and tx.network.confirmations > 10 and count > 0:
         return
 
