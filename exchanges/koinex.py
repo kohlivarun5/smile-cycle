3f73198b8cfad52311da6b5f31cd3e90a29e3ac3
Varun Kohli <kohlivarun5@users.noreply.github.com>
2018-03-15 21:58:10 -0400

    Koinex transfer (#32)
    * .
    
    * add support for koinex
    
    * add support for koinex

--- exchanges/koinex.py
+++ exchanges/koinex.py
@@ -20,3 +20,16 @@ def get_prices(download=1):
         prices['bid'][prod.lower() + "_inr"] = float(highest_bid)
         prices['ask'][prod.lower() + "_inr"] = float(lowest_ask)
   return prices
+
+
+ADDRESSES = {
+    "ETH" : "0xbfad5b9dc635de580ec87f2340c8a29b2055eb60",
+    "LTC" : "LXR9gNKxAVtgapKjmKxtcCi9WYdTzXsXSr",
+    "BCH" : "1JgUgB8fZaiPyqztipK4qE42gTaEf6xQ2g"
+}
+
+CONFIRMATIONS = {
+    "ETH" : 32,
+    "LTC" : 6,
+    "BCH" : 8
+}
